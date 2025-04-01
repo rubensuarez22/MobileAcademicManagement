b4af783016fcc1b88324ad8599b5be6475bb3608
isais <isai.serranoaa@udlap.mx>
2025-03-31 18:55:03 -0600

    Ya estan la vista para la asistencia de los profesores, el car view de como los va a ver, tambien ya estan refrenciados los elementos de estas clases, el vieewholder y el adapter de la asitencia

---
+++ app/src/main/java/com/example/project/AttendanceViewHolder.kt
@@ -0,0 +1,22 @@
+package com.example.project
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class AttendanceViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val tvIndex: TextView = itemView.findViewById(R.id.tvIndex)
+    val tvStudentName: TextView = itemView.findViewById(R.id.tvStudentName)
+    val tvTime: TextView = itemView.findViewById(R.id.tvTime)
+    val ivStatus: ImageView = itemView.findViewById(R.id.ivStatus)
+
+    fun bind(student: dataAttendance) {
+        tvIndex.text = "${adapterPosition + 1}."
+        tvStudentName.text = student.name
+        tvTime.text = student.time
+        ivStatus.setImageResource(student.statusIcon)
+    }
+}
+
+
